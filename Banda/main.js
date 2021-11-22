ea79661f6b2460995e05977a5857d6e5dcd59bac
Brenda Christine <brendachristinne@gmail.com>
2021-11-21 22:05:35 -0300

    Banda favorita

---
+++ Banda/main.js
@@ -0,0 +1,6 @@
+document.getElementById("titulo").innerHTML = "Olá, sou Brenda Christine"
+document.getElementById("banda").innerHTML = "Minha banda favorita é Guns n Roses"
+var idade = document.querySelectorAll('idade')
+console.log(idade);
+var idade = document.querySelectorAll('descricao')
+console.log(idade);
